8115454d275860b9e8f2be95ff33961b029f458c
vchiSBU <vincent.chi@stonybrook.edu>
2021-04-06 17:04:08 -0400

    Login and Create Account Modals

---
+++ client/src/components/modals/CreateAccount.js
@@ -0,0 +1,95 @@
+import React, { useState } 	from 'react';
+import { REGISTER }			from '../../cache/mutations';
+import { useMutation }    	from '@apollo/client';
+
+import { WModal, WMHeader, WMMain, WMFooter, WButton, WInput, WRow, WCol } from 'wt-frontend';
+
+const CreateAccount = (props) => {
+	const [input, setInput] = useState({ email: '', password: '', firstName: '', lastName: '' });
+	const [loading, toggleLoading] = useState(false);
+	const [Register] = useMutation(REGISTER);
+
+	const [isVisible, setVisible] = useState(true);
+
+	
+	const updateInput = (e) => {
+		const { name, value } = e.target;
+		const updated = { ...input, [name]: value };
+		setInput(updated);
+	};
+
+	const handleCreateAccount = async (e) => {
+		for (let field in input) {
+			if (!input[field]) {
+				alert('All fields must be filled out to register');
+				return;
+			}
+		}
+		const { loading, error, data } = await Register({ variables: { ...input } });
+		if (loading) { toggleLoading(true) };
+		if (error) { return `Error: ${error.message}` };
+		if (data) {
+			console.log(data)
+			toggleLoading(false);
+			if(data.register.email === 'already exists') {
+				alert('User with that email already registered');
+			}
+			else {
+				props.fetchUser();
+			}
+			props.setShowCreate(false);
+
+		};
+	};
+
+	return (
+        // Replace div with WModal
+		<div className="signup-modal">
+			<WModal visible = {isVisible}>
+				<WMHeader>
+					<div>
+						<WRow className="modal-col-gap signup-modal">
+							<WCol size="6">
+								<WInput 
+									className="" onBlur={updateInput} name="firstName" labelAnimation="up" 
+									barAnimation="solid" labelText="First Name" wType="outlined" inputType="text" 
+								/>
+							</WCol>
+							<WCol size="6">
+								<WInput 
+									className="" onBlur={updateInput} name="lastName" labelAnimation="up" 
+									barAnimation="solid" labelText="Last Name" wType="outlined" inputType="text" 
+								/>
+							</WCol>
+						</WRow>
+
+						<div className="modal-spacer">&nbsp;</div>
+						<WInput 
+							className="modal-input" onBlur={updateInput} name="email" labelAnimation="up" 
+							barAnimation="solid" labelText="Email Address" wType="outlined" inputType="text" 
+						/>
+						<div className="modal-spacer">&nbsp;</div>
+						<WInput 
+							className="modal-input" onBlur={updateInput} name="password" labelAnimation="up" 
+							barAnimation="solid" labelText="Password" wType="outlined" inputType="password" 
+						/>
+					</div>
+				</WMHeader>
+
+				<WMFooter>
+					<WButton className="modal-button" onClick={handleCreateAccount} span clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="primary">
+						Submit
+					</WButton>
+					<WButton className="modal-button" onClick = {() => {
+																setVisible(false);
+																props.setShowCreate(false)}
+																} span clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="transparent">
+						Cancel
+					</WButton>
+				</WMFooter>
+			</WModal>
+		</div>
+	);
+}
+
+export default CreateAccount;
